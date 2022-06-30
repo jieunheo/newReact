2b390ee54a97af4b13dc7368d9fe6aa6ddac059c
heojieun <gjekdmlwl@naver.com>
2022-06-30 15:29:56 +0900

    초기화

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom"
-import Header from './Components/Header'
-import Footer from './Components/Footer'
-import Home from './Views/Home'
-import About from './Views/About'
-import Product from './Views/Product'
+
+import HelloWorld from './Components/HelloWorld'
 
 function App() {
   return (
-    <div className="relative pb-10 min-h-screen">
-      <Router>
-        
-        <Header />
-
-        <div className="p-3">
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/products/:id">
-            <Product />
-          </Route>
-        </Switch>
-        </div>
-
-        <Footer />
-
-      </Router>
+    <div>
+      <HelloWorld name={"jieun"} />
     </div>
   );
 }
