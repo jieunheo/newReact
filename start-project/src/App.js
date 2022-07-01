fb271feb555c929dded9c48bc16dc0471646c9ef
heojieun <gjekdmlwl@naver.com>
2022-07-01 16:32:42 +0900

    초기 프로젝트와 완료 프로젝트 구분

---
+++ start-project/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HelloWorld from './Components/HelloWorld'
+
+function App() {
+  return (
+    <div>
+      <HelloWorld name="Quentin"/>
+    </div>
+  );
+}
+
+export default App;
